05cd9b11adf9305b227db2ad73e1859d5709456b
David Fraser <davidf@j5int.com>
2016-04-25 09:06:50 +0200

    Adjust formatting of cells so they round-trip exactly Add weekly summary to summary sheet Rather than using `get_all_records` method which doesn't give us access to cells to update, read entire spreadsheet into cell objects, then update in batches. This is much faster... Give fewer updates along the way

--- toggl_sheet_sync.py
+++ toggl_sheet_sync.py
@@ -48,6 +48,11 @@ def get_or_add_worksheet(spreadsheet, sheet_name, rows=1000, cols=20):
         return spreadsheet.add_worksheet(sheet_name, rows, cols)
 
 SHEET_HEADERS = ['Date', 'toggl_id', 'Start', 'End', 'Project', 'Description', 'Duration']
+SUMMARY_HEADERS = ['Week Start', 'Days Worked', 'Total Hours']
+
+def format_time(t):
+    s = t.strftime("%H:%M").lstrip('0')
+    return '0' + s if s.startswith(':') else s
 
 def entry_to_sheet_row(entry):
     start_time = dateutil.parser.parse(entry.get('start')) if entry.get('start') else None
@@ -60,9 +65,9 @@ def entry_to_sheet_row(entry):
     return {
         'Date': start_time.strftime('%Y-%m-%d'),
         'toggl_id': entry.get('id'),
-        'Start': start_time.strftime('%H:%M'),
-        'End': end_time.strftime('%H:%M'),
-        'Project': project.get('name') if project else None,
+        'Start': format_time(start_time),
+        'End': format_time(end_time),
+        'Project': "'" + project.get('name') if project else None,
         'Description': "'" + entry.get('description'),
         'Duration': '%d:%02d' % divmod(duration//60, 60)
     }
@@ -71,10 +76,10 @@ def cell_name(row, col):
     col_name = chr(ord('A') + col-1) if col <= 26 else (chr(ord('A') + (col-1)/26) + chr(ord('A') + (col-1) % 26))
     return "%s%d" % (col_name, row)
 
-def setup_header(worksheet):
+def setup_header(worksheet, headers=SHEET_HEADERS):
     header_cells = []
     header_row = worksheet.row_values(1)
-    for n, header in enumerate(SHEET_HEADERS):
+    for n, header in enumerate(headers):
         h_value = header_row[n] if len(header_row) > n else None
         if not h_value:
             h = worksheet.cell(1, n+1)
@@ -86,47 +91,97 @@ def setup_header(worksheet):
     if header_cells:
         worksheet.update_cells(header_cells)
 
+def start_of_week(d):
+    """Returns the starting day of the week for the given datetime"""
+    return (d - timedelta(days=d.weekday())).date()
+
 def sync_sheets(spreadsheet, year, client=None):
     today = datetime.today()
     months = range(1, today.month+1 if today.year == year else 13)
+    summary_sheet = get_or_add_worksheet(spreadsheet, "Summary")
+    setup_header(summary_sheet, SUMMARY_HEADERS)
+    weeks = {}
     for month in months:
         start_date = datetime(year=year, month=month, day=1)
         end_date = (start_date + timedelta(days=32)).replace(day=1)
         month_sheet = get_or_add_worksheet(spreadsheet, start_date.strftime("%b"))
         setup_header(month_sheet)
-        sheet_rows = month_sheet.get_all_records()
-        toggl_id_map = {row['toggl_id']: (n+1, row) for (n, row) in enumerate(sheet_rows) if row['toggl_id']}
-        append_row = len(sheet_rows)+1
+        toggl_id_map = {}
+        append_row = 2
         update_cells = []
         added, updated = 0, 0
-        for time_entry in get_entries(start_date, end_date, client):
+        logging.info("Fetching toggl data for %s", month_sheet.title)
+        month_entries = list(get_entries(start_date, end_date, client))
+        logging.info("Fetching existing spreadsheet data for %s", month_sheet.title)
+        full_range = month_sheet.range("%s:%s" % (month_sheet.get_addr_int(2, 1),
+                                                  month_sheet.get_addr_int(month_sheet.row_count, len(SHEET_HEADERS))))
+        def get_row(row_num):
+            if 2 <= row_num <= month_sheet.row_count:
+                start_cell = (row_num-2)*len(SHEET_HEADERS)
+                cells = full_range[start_cell:start_cell+len(SHEET_HEADERS)]
+            else:
+                cells = month_sheet.range("%s:%s" % (month_sheet.get_addr_int(row_num, 1),
+                                                     month_sheet.get_addr_int(row_num, len(SHEET_HEADERS))))
+            assert len(cells) == len(SHEET_HEADERS)
+            for n, cell in enumerate(cells):
+                assert cell.row == row_num
+                assert cell.col == n + 1
+            return cells
+
+        for row_num in range(2, month_sheet.row_count+1):
+            row_cells = get_row(row_num)
+            sheet_row = {SHEET_HEADERS[n]: row_cells[n].value for n in range(len(SHEET_HEADERS))}
+            if sheet_row['toggl_id']:
+                toggl_id_map[int(sheet_row['toggl_id'])] = row_num, sheet_row
+                if row_num >= append_row:
+                    append_row = row_num + 1
+        logging.info("Synchronizing data")
+        for time_entry in month_entries:
+            if time_entry.get('start'):
+                week = start_of_week(dateutil.parser.parse(time_entry.get('start')))
+                weeks.setdefault(week, 0)
+                weeks[week] += time_entry.get('duration')
             toggl_id = time_entry.get('id')
             sheet_values = entry_to_sheet_row(time_entry)
-            cell_list = month_sheet.range("%s:%s" % (month_sheet.get_addr_int(append_row, 1),
-                                                     month_sheet.get_addr_int(append_row, len(SHEET_HEADERS) + 1)))
             if toggl_id in toggl_id_map:
                 row, sheet_row = toggl_id_map[toggl_id]
+                cell_list = get_row(row)
                 for header, update_cell in zip(SHEET_HEADERS, cell_list):
-                    if sheet_row[header] != sheet_values[header]:
+                    cell_value = sheet_row[header]
+                    if header == 'toggl_id':
+                        cell_value = int(cell_value)
+                    elif header in ("Project", "Description") and cell_value:
+                        cell_value = "'" + cell_value
+                    if cell_value != sheet_values[header]:
                         update_cell.value = sheet_values[header]
+                        logging.info("Mismatch on %s: %r %r", header, cell_value, sheet_values[header])
                         update_cells.append(update_cell)
                 updated += 1
             else:
-                append_row += 1
-                cell_list = month_sheet.range("%s:%s" % (month_sheet.get_addr_int(append_row, 1),
-                                                         month_sheet.get_addr_int(append_row, len(SHEET_HEADERS)+1)))
+                cell_list = get_row(append_row)
                 for header, update_cell in zip(SHEET_HEADERS, cell_list):
                     if update_cell is not None:
                         update_cell.value = sheet_values[header]
                 update_cells.extend(cell_list)
                 added += 1
-            logging.info("Added %d, updated %d rows", added, updated)
-            if len(update_cells) > 100:
+                append_row += 1
+            if (added + updated) % 100 == 0:
+                logging.info("Added %d, updated %d rows", added, updated)
+            if len(update_cells) > 250:
                 logging.info("Sending %d cells to sheet", len(update_cells))
                 month_sheet.update_cells(update_cells)
                 del update_cells[:]
-        month_sheet.update_cells(update_cells)
-    summary_sheet = get_or_add_worksheet(spreadsheet, "Summary")
+        if (update_cells):
+            logging.info("Sending %d cells to sheet", len(update_cells))
+            month_sheet.update_cells(update_cells)
+    week_cells = summary_sheet.range("%s:%s" % (summary_sheet.get_addr_int(2, 1),
+                                                summary_sheet.get_addr_int(len(weeks)+2, len(SUMMARY_HEADERS))))
+    for n, (week, duration) in enumerate(sorted(weeks.items())):
+        minutes = duration // 60
+        week_cells[n * len(SUMMARY_HEADERS) + 0].value = week.strftime("%Y-%m-%d")
+        week_cells[n * len(SUMMARY_HEADERS) + 2].value = "%d:%02d" % (minutes//60, minutes%60)
+    summary_sheet.update_cells(week_cells)
+
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
